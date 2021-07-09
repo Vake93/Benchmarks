4046e89f009b356364dc7ec81e4c33c2983c2ec9
Vishvaka <vishvaka@gmail.com>
2021-07-09 22:00:34 +0530

    Added C# Benchmark

---
+++ cs-asp-pg/Fortune.cs
@@ -0,0 +1,9 @@
+﻿namespace benchmark
+{
+    public class Fortune
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ cs-asp-pg/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using System.Data;
+
+namespace benchmark
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(_configuration.GetConnectionString("postgres")));
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    using var connection = context.RequestServices.GetRequiredService<IDbConnection>();
+                    var results = await connection.QueryAsync<Fortune>("SELECT * FROM fortune");
+                    await context.Response.WriteAsJsonAsync(results);
+                });
+            });
+        }
+    }
+}
