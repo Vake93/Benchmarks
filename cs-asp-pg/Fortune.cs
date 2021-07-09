0dc62b255b9e7605812800dd6b1ed35f43836735
Vishvaka <vishvaka@gmail.com>
2021-07-09 22:09:15 +0530

    Simplified C# Code

--- cs-asp-pg/Fortune.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace benchmark
-{
-    public class Fortune
-    {
-        public int Id { get; set; }
-
-        public string Message { get; set; }
-    }
-}
--- cs-asp-pg/Program.cs
+++ cs-asp-pg/Program.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Data;
+using Npgsql;
+using Dapper;
 
-namespace benchmark
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) => Host
+    .CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+    .ConfigureLogging(logBuilder => logBuilder.ClearProviders());
+
+record Fortune (int Id, string Message);
+
+class Startup
 {
-    public class Program
-    {
-        public static void Main(string[] args) => CreateHostBuilder(args)
-            .Build()
-            .Run();
+    private readonly IConfiguration _configuration;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) => Host
-            .CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-            .ConfigureLogging(logBuilder => logBuilder.ClearProviders());
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(_configuration.GetConnectionString("postgres")));
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                using var connection = context.RequestServices.GetRequiredService<IDbConnection>();
+                var results = await connection.QueryAsync<Fortune>("SELECT * FROM fortune");
+                await context.Response.WriteAsJsonAsync(results);
+            });
+        });
     }
 }
