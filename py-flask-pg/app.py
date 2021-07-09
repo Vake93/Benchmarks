bc33808098394fd24aad89a66cb0d2935c8026a2
Vishvaka <vishvaka@gmail.com>
2021-07-09 10:36:40 +0530

    Fixed database connection issues

--- py-flask-pg/app.py
+++ py-flask-pg/app.py
@@ -1,16 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify
-from models import Fortune
+from models import Fortune, db
+from waitress import serve
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+db.init_app(app)
+
 
 @app.route('/')
 def hello():
     fortunes = Fortune.query.all()
     return jsonify([f.serialize for f in fortunes])
 
+
 if __name__ == '__main__':
-    app.run()
+    serve(app, host="0.0.0.0", port=5000, threads=8)
