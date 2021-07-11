362fe4de1dc5b97df4803ce01530b7bc04d281c7
Vishvaka <vishvaka@gmail.com>
2021-07-11 15:30:41 +0530

    Updated JS Benchmark

--- js-express-pg/index.js
+++ js-express-pg/index.js
@@ -5,16 +5,30 @@ const app = express();
 const port = 5000;
 
 const pool = new Pool({
-    connectionString: "postgresql://user:user@123@localhost:5432/fortunes?sslmode=disable",
-    max: 5,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionString: "postgresql://user:user@123@localhost:5432/fortunes?sslmode=disable"
 })
 
 app.use(express.json());
 
-app.get('/', async (request, response) => {
-    var results = await pool.query("SELECT id, message FROM fortune");
+app.get('/text', (_, response) => {
+    response.status(200).send("Hello World");
+});
+
+app.get('/json', (_, response) => {
+    response.status(200).json({
+        id: 1,
+        message: "Hello World",
+    });
+});
+
+app.get('/single', async (_, response) => {
+    const results = await pool.query("SELECT id, message FROM fortune ORDER BY id");
+    response.status(200).json(results.rows);
+});
+
+app.get('/multiple', async (_, response) => {
+    const item = await pool.query("SELECT id, randomnumber FROM world WHERE id = 260").then(r => r.rows[0]);
+    const results = await pool.query("SELECT id, randomnumber FROM world WHERE randomnumber > $1 ORDER BY id", [item.randomnumber]);
     response.status(200).json(results.rows);
 });
 
