30f33e28672505a1c592b8dd66f1fffe224da0c1
Vishvaka <vishvaka@gmail.com>
2021-07-11 14:00:40 +0530

    Updated C# benchmark

--- cs-asp-pg/Program.cs
+++ cs-asp-pg/Program.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
+﻿using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Npgsql;
 using System.Data;
 using System.IO;
-using Npgsql;
-using Dapper;
 
 var config = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
@@ -32,6 +32,8 @@ builder.Build().Run();
 
 record Fortune(int Id, string Message);
 
+record World(int Id, int RandomNumber);
+
 class Startup
 {
     private readonly IConfiguration _configuration;
@@ -53,10 +55,22 @@ class Startup
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("/", async context =>
+            endpoints.MapGet("/text", context => context.Response.WriteAsync("Hello World"));
+
+            endpoints.MapGet("/json", context => context.Response.WriteAsJsonAsync(new Fortune(1, "Hello World")));
+
+            endpoints.MapGet("/single", async context =>
             {
                 var connection = context.RequestServices.GetRequiredService<IDbConnection>();
-                var results = await connection.QueryAsync<Fortune>("SELECT * FROM fortune");
+                var results = await connection.QueryAsync<Fortune>("SELECT id, message FROM fortune ORDER BY id");
+                await context.Response.WriteAsJsonAsync(results);
+            });
+
+            endpoints.MapGet("/multiple", async context =>
+            {
+                var connection = context.RequestServices.GetRequiredService<IDbConnection>();
+                var item = await connection.QuerySingleAsync<World>("SELECT id, randomnumber FROM world WHERE id = 260");
+                var results = await connection.QueryAsync<World>("SELECT id, randomnumber FROM world WHERE randomnumber > @RandomNumber ORDER BY id", item);
                 await context.Response.WriteAsJsonAsync(results);
             });
         });
