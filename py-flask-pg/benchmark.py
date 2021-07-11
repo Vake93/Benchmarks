9eaf67a6d2a3a6adc1a764974791aabea6525164
Vishvaka <vishvaka@gmail.com>
2021-07-11 16:01:35 +0530

    Updated PY Benchmark

--- py-flask-pg/benchmark.py
+++ py-flask-pg/benchmark.py
@@ -10,10 +10,30 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
-@app.route('/')
-def hello():
+@app.route('/text')
+def text_handler():
+    return "Hello World"
+
+
+@app.route('/json')
+def json_handler():
+    return jsonify({
+        "id": 1,
+        "message": "Hello World",
+    })
+
+
+@app.route('/single')
+def single_handler():
     fortunes = Fortune.query.all()
     return jsonify([f.serialize for f in fortunes])
+
+
+@app.route('/multiple')
+def multiple_handler():
+    item = World.query.filter_by(id=260).first()
+    results = World.query.filter(World.random_number > item.random_number).all()
+    return jsonify([r.serialize for r in results])
 
 
 class Fortune(db.Model):
@@ -31,6 +51,24 @@ class Fortune(db.Model):
         return {
             'id': self.id,
             'message': self.message
+        }
+
+
+class World(db.Model):
+    __tablename__ = 'world'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    random_number = db.Column(db.Integer(), name="randomnumber")
+
+    def __init__(self, id, random_number):
+        self.id = id
+        self.random_number = random_number
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'randomNumber': self.random_number
         }
 
 
